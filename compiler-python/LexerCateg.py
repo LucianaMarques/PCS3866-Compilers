b3727fc4071d20e01462260f0647674d54fa955b
luciana <lucianadacostamarques@gmail.com>
2019-01-16 10:34:55 -0200

    changed token generation in next_state()

--- compiler-python/LexerCateg.py
+++ compiler-python/LexerCateg.py
@@ -40,118 +40,117 @@ class LexerCategorizer:
             self.next_state(i)
 
     def next_state(self,i):
-        # # if we found an "EOL" on the diagram
-        # if (self.characters[i].type == "descartavel" or self.characters[i].type == "controle"):
-        #     if (self.automaton_state.id == 1):
-        #         self.generate_token(= "character"
-        #     elif (self.automaton_state.id == ):
-        #         if (read.size == 2):
-        #             type = "identifier"
-        #         else:
-        #             type = "character"
-        #     elif (self.automaton_state.id == 5):
-        #         type = "int"
-        #     elif (self.automaton_state.id == 11):
-        #         if (read.begin == "+" or read.begin == "-"):
-        #             type = "snum"
-        #         else:
-        #             type = "num"
-        #     self.generate_token(type,read)
-
-        # if current state is 1 in the diagram
-        # else:
-        self.automaton_state.read = self.automaton_state.read + self.characters[i].char
-        # print(self.automaton_state.read)
-        if (self.automaton_state.id == 1):
-            if (self.characters[i].type == "letter"):
-                reserved = self.check_reserved()
-                composed = self.check_composed()
-                if (not reserved and not composed):
-                    if (characters[i+1].type == "digit"):
-                        self.automaton_state.id = 3
-                    else:
-                        self.automaton_state.id = 4
-                    
-                elif (reserved):
-                    pass
-                elif (composed):
-                    pass
-
-            elif (self.characters[i].type == "digit"):
-                self.automaton_state.id = 4
-            elif (self.characters[i].type == "special"):
-                self.automaton_state.id = 4
-            elif (self.characters[i].char == "+" or self.characters[i].char == "-"):
-                self.automaton_state.id = 5
-            elif (self.characters[i].char == "."):
-                self.automaton_state.id = 6
-            else:
-                self.automaton_state.id = 6
-        
-        elif (self.automaton_state.id == 3):
-            self.generate_token("IDENTIFIER", automaton_state.red)
-            self.automaton_state.id = 1
-            self.automaton_state.read = ""
-        
-        # if current state is 4 and we're not considering buggy codes, it must be a character
-        elif (self.automaton_state.id == 4):
-            self.generate_token("CHARACTER", self.automaton_state.read)
-            self.automaton_state.read = ""
-
-        elif (self.automaton_state.id == 5):
-            self.automaton_state.id = 6
-        
-        elif (self.automaton_state.id == 6):
-            self.automaton_state.id = 7
-        
-        elif (self.automaton_state.id == 7):
-            if (self.characters[i+1].type == "descartavel" or self.characters[i+1] == "controle"):
+        # if we found an "EOL" on the diagram
+        if (self.characters[i].type == "descartavel" or self.characters[i].type == "controle"):
+            if (self.automaton_state.id == 4):
+                self.generate_token("CHARACTER", self.automaton_state.read)
+            elif (self.automaton_state.id == ):
+                if (read.size == 2):
+                    self.generate_token("IDENTIFIER", automaton_state.red)
+                else:
+                    self.generate_token("CHARACTER", self.automaton_state.read)
+            elif (self.automaton_state.id == 5):
                 self.generate_token("INT", self.automaton_state.read)
-                self.automaton_state.id = 1
-                self.automaton_state.read = ""
-            elif (self.characters[i+1].type == "digit"):
-                pass
-            elif (self.characters[i+1].char == "."):
-                self.automaton_state.id = 8
-            else:
-                self.automaton_state.id = 9
-        
-        elif (self.automaton_state.id == 8):
-            if (self.characters[i+1].type == "digit"):
-                pass
-            else:
-                self.automaton_state.id = 9
-        
-        elif (self.automaton_state.id == 9):
-            if (self.characters[i+1].char == "E"):
-                self.automaton_state.id = 10
-            else:
-                self.automaton_state.id = 13
-        
-        elif (self.automaton_state.id == 10):
-            self.automaton_state.id = 11
+            elif (self.automaton_state.id == 11):
+                if (read.begin == "+" or read.begin == "-"):
+                    self.generate_token("SNUM", self.automaton_state.read)
+                else:
+                    self.generate_token("NUM", self.automaton_state.read)
 
-        elif (self.automaton_state.id == 11):
-            self.automaton_state.id = 12
-        
-        elif (self.automaton_state.id == 12):
-            if (self.characters[i+1].type == "digit"):
-                pass
-            else:
-                self.automaton_state.id = 13
-        
-        elif (self.automaton_state.id == 13):
-            if (self.automaton_state.read[0] == "+" or self.automaton_state.read[0] == "-"):
-                self.generate_token("SNUM", automaton_state.read)
-                self.automaton_state.id = 1
-                self.automaton_state.read = ""
-            else:
-                self.generate_token("NUM", self.automaton_state.read)
-                self.automaton_state.id = 1
-                self.automaton_state.read = ""
-        
+        if current state is 1 in the diagram
         else:
-            pass
+            self.automaton_state.read = self.automaton_state.read + self.characters[i].char
+            # print(self.automaton_state.read)
+            if (self.automaton_state.id == 1):
+                if (self.characters[i].type == "letter"):
+                    reserved = self.check_reserved()
+                    composed = self.check_composed()
+                    if (not reserved and not composed):
+                        if (characters[i+1].type == "digit"):
+                            self.automaton_state.id = 3
+                        else:
+                            self.automaton_state.id = 4
+                        
+                    elif (reserved):
+                        pass
+                    elif (composed):
+                        pass
+
+                elif (self.characters[i].type == "digit"):
+                    self.automaton_state.id = 4
+                elif (self.characters[i].type == "special"):
+                    self.automaton_state.id = 4
+                elif (self.characters[i].char == "+" or self.characters[i].char == "-"):
+                    self.automaton_state.id = 5
+                elif (self.characters[i].char == "."):
+                    self.automaton_state.id = 6
+                else:
+                    self.automaton_state.id = 6
+            
+            elif (self.automaton_state.id == 3):
+                self.generate_token("IDENTIFIER", automaton_state.red)
+                self.automaton_state.id = 1
+                self.automaton_state.read = ""
+            
+            # if current state is 4 and we're not considering buggy codes, it must be a character
+            elif (self.automaton_state.id == 4):
+                self.generate_token("CHARACTER", self.automaton_state.read)
+                self.automaton_state.read = ""
+
+            elif (self.automaton_state.id == 5):
+                self.automaton_state.id = 6
+            
+            elif (self.automaton_state.id == 6):
+                self.automaton_state.id = 7
+            
+            elif (self.automaton_state.id == 7):
+                if (self.characters[i+1].type == "descartavel" or self.characters[i+1] == "controle"):
+                    self.generate_token("INT", self.automaton_state.read)
+                    self.automaton_state.id = 1
+                    self.automaton_state.read = ""
+                elif (self.characters[i+1].type == "digit"):
+                    pass
+                elif (self.characters[i+1].char == "."):
+                    self.automaton_state.id = 8
+                else:
+                    self.automaton_state.id = 9
+            
+            elif (self.automaton_state.id == 8):
+                if (self.characters[i+1].type == "digit"):
+                    pass
+                else:
+                    self.automaton_state.id = 9
+            
+            elif (self.automaton_state.id == 9):
+                if (self.characters[i+1].char == "E"):
+                    self.automaton_state.id = 10
+                else:
+                    self.automaton_state.id = 13
+            
+            elif (self.automaton_state.id == 10):
+                self.automaton_state.id = 11
+
+            elif (self.automaton_state.id == 11):
+                self.automaton_state.id = 12
+            
+            elif (self.automaton_state.id == 12):
+                if (self.characters[i+1].type == "digit"):
+                    pass
+                else:
+                    self.automaton_state.id = 13
+            
+            elif (self.automaton_state.id == 13):
+                if (self.automaton_state.read[0] == "+" or self.automaton_state.read[0] == "-"):
+                    self.generate_token("SNUM", automaton_state.read)
+                    self.automaton_state.id = 1
+                    self.automaton_state.read = ""
+                else:
+                    self.generate_token("NUM", self.automaton_state.read)
+                    self.automaton_state.id = 1
+                    self.automaton_state.read = ""
+            
+            else:
+                pass
       
     def check_reserved(self):
         pass
